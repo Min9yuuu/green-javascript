4eb9dc82ec96669d808ea7ffeadb9fe7195d6f17
M9 <9yu6820@gmail.com>
2022-12-30 16:55:39 +0900

    slide button

--- 03/js/table.js
+++ 03/js/table.js
@@ -1,10 +1,10 @@
-let menNum=prompt("몇명이 앉을 지 정해주세요");
-let colNum=prompt("한줄에 몇개의 좌석을 배치하시겠습니까?");
+const menNum = prompt("몇명이 앉을 지 정해주세요");
+const colNum = prompt("한줄에 몇개의 좌석을 배치하시겠습니까?");
 let count = 0;
 let rowNum;
 
 if(menNum%colNum===0){
-  (rowNum) = parseInt(menNum / colNum);
+  rowNum = parseInt(menNum / colNum);
 }
 else{
   rowNum = parseInt((menNum/ colNum)+1);
@@ -12,17 +12,15 @@ else{
 
 document.write(`<h2>총 필요한 줄의 갯수는 ${rowNum}입니다.</h2>`);
 document.write('<table>');
-
 for(let i = 1; i<=rowNum;i++){
   document.write("<tr>");
   for(let j = 1; j<=colNum; j++){
     count += 1;
     if(count <=menNum){
       document.write(`<td>좌석${count}</td>`);
-    }else{
+    }
+    else{
       document.write(`<td></td>`);
     }
   }
-  document.write("</tr>")
 }
-
--- 04/js/slide01.js
+++ 04/js/slide01.js
@@ -1,6 +1,6 @@
 $(function(){
   const visual = $("#brandVisual>ul>li"),
-    btn = $("#brandVisual>ul>li");
+    btn = $("#buttonList>li");
     
     let current = 0;//current: 현재
     let id; // setIntervalID
@@ -8,26 +8,48 @@ $(function(){
 
     // 버튼클릭함수
     btn.click(function(){
-
+      // jQuery 문법에서 this를 그냥 쓸경우 jQuery함수를 사용 못한다.(this는 javaScript문법)
+      // jQuery 문법에서 this를 사용하고 싶으면 $(this)를 사용, javaScript문법만 사용하는경우는 this로 사용해도 된다.
+      let btnIdx = $(this).index();
+      /* 
+        선택된 것만 적용하기
+          1. 다 지우고 선택된 것만 적용하기
+      */
+      btn.removeClass("on");
+      $(this).addClass("on");
+      move(btnIdx);
     });
+
     // 시간마다 실행
     timer();
     function timer(){
-      setInterval(function(){
+      id = setInterval(function(){
         let next = current+1;
-        if(next==visual.length-1){
+        if(next==visual.length){
           next=0;
         }
-        move(next);
+        console.log(next);
+        btn.eq(next).trigger("click");
       },speed);
     }
+        
+
     // 이동
-    function move(next){
-      console.log("next" + next);
+    function move(btnIdx){
+      if(btnIdx==current)return;
+      console.log("무브" + current);
       let cu=visual.eq(current);
-      let ne = visual.eq(next);
+      let ne = visual.eq(btnIdx);
       cu.css("left","0").stop().animate({left:"-100%"});
-      ne.stop().animate({left:"0%"});
+      ne.css("left","100%").stop().animate({left:"0%"});
+      current=btnIdx;
     }
+
+    $("#brandVisual,#buttonList").mouseenter(function () {
+      clearInterval(id);
+    });
+    $("#brandVisual,#buttonList").mouseleave(function () {
+      timer();
+    });
 
 });//jQuery
